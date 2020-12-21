c23443b24d5135204b490d1b9c17b401e3572213
Daniel Carbajal <dcarbajal27@my365.bellevue.edu>
2020-12-20 23:40:23 -0800

    Update chap01ex.py

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -1,26 +1,147 @@
 """This file contains code for use with "Think Stats",
 by Allen B. Downey, available from greenteapress.com
 
-Copyright 2014 Allen B. Downey
+Copyright 2010 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
-from __future__ import print_function
+from __future__ import print_function, division
 
-import numpy as np
 import sys
-
-import nsfg
+import numpy as np
 import thinkstats2
+import matplotlib.pyplot as plt
+import nsfg
 
 
-def main(script):
+from collections import defaultdict
+
+
+def ReadFemPreg(dct_file='2002FemPreg.dct',
+                dat_file='2002FemPreg.dat.gz'):
+    """Reads the NSFG pregnancy data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    CleanFemPreg(df)
+    return df
+
+
+def CleanFemPreg(df):
+    """Recodes variables from the pregnancy frame.
+
+    df: DataFrame
+    """
+    # mother's age is encoded in centiyears; convert to years
+    df.agepreg /= 100.0
+
+    # birthwgt_lb contains at least one bogus value (51 lbs)
+    # replace with NaN
+    df.loc[df.birthwgt_lb > 20, 'birthwgt_lb'] = np.nan
+
+    # replace 'not ascertained', 'refused', 'don't know' with NaN
+    na_vals = [97, 98, 99]
+    df.birthwgt_lb.replace(na_vals, np.nan, inplace=True)
+    df.birthwgt_oz.replace(na_vals, np.nan, inplace=True)
+    df.hpagelb.replace(na_vals, np.nan, inplace=True)
+
+    df.babysex.replace([7, 9], np.nan, inplace=True)
+    df.nbrnaliv.replace([9], np.nan, inplace=True)
+
+    # birthweight is stored in two columns, lbs and oz.
+    # convert to a single column in lb
+    # NOTE: creating a new column requires dictionary syntax,
+    # not attribute assignment (like df.totalwgt_lb)
+    df['totalwgt_lb'] = df.birthwgt_lb + df.birthwgt_oz / 16.0
+
+
+    #print(df['totalwgt_lb'])
+    #df['totalwgt_lb'].histogram(bins=10)
+    # An "interface" to matplotlib.axes.Axes.hist() method
+    plt.hist(x=df['totalwgt_lb'],bins=20, color='#0504aa',
+                            alpha=0.7, rwidth=1.0)
+    plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('Weight (lb)')
+    plt.ylabel('Frequency')
+    plt.title('Number of Babies At Each Weight')
+    plt.text(23, 45, r'$\mu=15, b=3$')
+    plt.show()
+
+
+    # due to a bug in ReadStataDct, the last variable gets clipped;
+    # so for now set it to NaN
+    df.cmintvw = np.nan
+
+
+def ValidatePregnum(resp, preg):
+    """Validate pregnum in the respondent file.
+
+    resp: respondent DataFrame
+    preg: pregnancy DataFrame
+    """
+    # make the map from caseid to list of pregnancy indices
+    preg_map = MakePregMap(preg)
+
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
+
+
+def MakePregMap(df):
+    """Make a map from caseid to list of preg indices.
+
+    df: DataFrame
+
+    returns: dict that maps from caseid to list of indices into `preg`
+    """
+    d = defaultdict(list)
+    for index, caseid in df.caseid.iteritems():
+        d[caseid].append(index)
+    return d
+
+
+def main():
     """Tests the functions in this module.
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    #esp = ReadFemResp()
+
+    #assert(len(resp) == 7643)
+   #assert(resp.pregnum.value_counts()[1] == 1267)
+
+    # read and validate the pregnancy file
+
+
+    preg = nsfg.ReadFemPreg()
+    data_preg = preg.head()
+    print(data_preg)
+    print(preg.columns)
+    print(preg.columns[0])
+    pregordr = preg['pregordr']
+    print(type(pregordr))
+    print(preg.birthwgt_lb.value_counts().sort_index())
+    preg = ReadFemPreg()
+
+
+
 
 
 if __name__ == '__main__':
-    main(*sys.argv)
+    main()
+
+
